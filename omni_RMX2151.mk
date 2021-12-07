7c0f2da3ec7eaedcc026de1adaaf2a75e37cb550
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2021-12-07 19:42:17 +0100

    Update omni_RMX2151.mk

--- omni_RMX2151.mk
+++ omni_RMX2151.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2151
